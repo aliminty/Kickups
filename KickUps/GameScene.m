9932db2bd49c310c4442b9715805802d78178d15
Ali Minty <aminty@ucdavis.edu>
2015-05-09 00:39:51 -0700

    vector accuracy, screen boundries
    -started work on making the force vector more accurate
    -also tried to fix issue where sides of screen are not physics boundries (has to do with spritekit scaling)

--- KickUps/GameScene.m
+++ KickUps/GameScene.m
@@ -18,6 +18,8 @@
 @synthesize score;
 @synthesize highScore;
 
+
+
 -(void)didMoveToView:(SKView *)view {
     
     //SKSpriteNode *soccerball = [SKSpriteNode spriteNodeWithImageNamed:@"Soccerball"];
@@ -37,7 +39,9 @@
     
     self.physicsWorld.gravity = CGVectorMake(0,-7);
     self.physicsWorld.contactDelegate = self;
-    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    CGRect theFrame = CGRectMake(self.anchorPoint.x, self.anchorPoint.y, self.size.width, self.size.height);
+    NSLog(@"view: %f,%f   scene: %f, %f", (double)(self.view.frame.size.width), (double)(self.view.frame.size.height), (double)(self.size.width), (double)(self.size.height));
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:theFrame];
     
     score = 0;
     highScore = 0;
@@ -49,6 +53,7 @@
     SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"Soccerball"];
     ball.physicsBody = [SKPhysicsBody bodyWithTexture:ball.texture size:ball.texture.size];
     ball.position = middlePoint;
+    //ball.size = CGSizeMake(ball.size.width*2, ball.size.height);
     ball.xScale = 0.5;
     ball.yScale = 0.5;
     ball.name = @"ball";
@@ -58,12 +63,32 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
+    CGFloat forceMultX = 50;
+    CGFloat forceMulty = 490;
+    
     for (UITouch *touch in touches) {
+        
         CGPoint location = [touch locationInNode:self];
+        CGFloat x1 = location.x;
+        CGFloat y1 = location.y;
+        CGVector ballVector;
+        
         SKSpriteNode *nodeTouched = (SKSpriteNode *)[self nodeAtPoint:location];
         if ([nodeTouched.name isEqual: @"ball"]) {
             NSLog(@"made contact with ball");
-            [nodeTouched.physicsBody applyImpulse: CGVectorMake(0, 4000) atPoint:location];
+            
+            // create vector for physics force (impulse)
+            CGFloat x2 = nodeTouched.position.x;
+            CGFloat y2 = nodeTouched.position.y;
+            NSLog(@"x1: %f, y1: %f, x2: %f, y2: %f", (double)x1, (double)y1, (double)x2, (double)y2);
+            if (y1 > y2) {
+                ballVector = CGVectorMake((x2-x1)*forceMultX, (y2-y1)*forceMulty);
+            }
+            else {
+                ballVector = CGVectorMake((x2-x1)*2*forceMultX, (y1-y2)*forceMulty);
+            }
+            
+            [nodeTouched.physicsBody applyImpulse: ballVector atPoint:location];
         }
         
         //SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
